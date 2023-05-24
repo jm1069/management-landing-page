f0c58f1c1ef24d866f528f72bd4172ed494f4f97
Jonas <78270099+jm1069@users.noreply.github.com>
2023-05-24 11:29:13 +0200

    refactor code + react router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,30 +7,34 @@ const Navbar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false)
 
   return (
-    <>
-        <div className='NavbarContainer'>
-            <div className='LogoContainer'>
-                <h1>Cranberry<span style={{color: '#b9a0ff'}}>.</span></h1>
-            </div>
-
-            <div className='LinksContainer'>
-                <p>Home</p>
-                <p>Plattform</p>
-                <p>Price</p>
-                <p>Community</p>
-            </div>
-
-            <div className='AccountButtons'>
-                <p>Login</p>
-                <button className='JoinUsButton'>Join Us</button>
-            </div>
-
-            <button className='Hamburger' onClick={() => {setIsNavExpanded(!isNavExpanded);console.log(isNavExpanded)}}><RxHamburgerMenu/></button>
-            <div className={isNavExpanded ? "expanded" : "hidden"}>
-                <p>expanded nav</p>
-            </div>
+    <div className='NavbarContainer'>
+        <div className='LogoContainer'>
+            <h1>Cranberry<span style={{color: '#b9a0ff'}}>.</span></h1>
         </div>
-    </>
+
+        <div className='LinksContainer'>
+            <p>Home</p>
+            <p>Plattform</p>
+            <p>Price</p>
+            <p>Community</p>
+        </div>
+
+        <div className='AccountButtons'>
+            <p>Login</p>
+            <button className='JoinUsButton'>Join Us</button>
+        </div>
+
+        <button 
+            className='Hamburger' 
+            onClick={() => {setIsNavExpanded(!isNavExpanded);console.log(isNavExpanded)}}
+        >
+            <RxHamburgerMenu/>
+        </button>
+
+        <div className={isNavExpanded ? "expanded" : "hidden"}>
+            <p>mobile nav soon</p>
+        </div>
+    </div>
   )
 }
 
